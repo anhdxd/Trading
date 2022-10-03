4cf0f714769a30a88b72adb7a6a7f711c8073740
anhdvd <anhdvd@bkav.com>
2022-10-03 13:51:19 +0700

    Get bóng nến tăng giảm

--- Analystic.py
+++ Analystic.py
@@ -2,9 +2,7 @@
 from ast import main
 from calendar import month
 from datetime import datetime, timedelta
-from mimetypes import init
 from msilib.schema import Class
-from operator import index
 from time import sleep
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,9 +12,11 @@ import os
 
 # DataAnalystic class ***********************************************
 
-
 class DataAnalystic(object):
-    
+    CU_up_name ="increase_up_sd"    # candle up - up shadows
+    CU_down_name = "increase_down_sd" # candle up - down shadows
+    CL_up_name = "decrease_up_sd"   # cander low - up shadows
+    CL_down_name = "decrease_down_sd" # candle low - down shadows
     def __init__(self, pathfile):
         self.pdReader = pd.read_csv(pathfile)
         self.SubCandle = (self.pdReader["open"] - self.pdReader["close"])*10000 
@@ -36,42 +36,53 @@ class DataAnalystic(object):
         print(scope)
         return scope
 
-    def UpperShadows_Analystic(self, CandleRange=10, Maxpip=500):
+    def IncreaseShadows_Analystic(self, CandleRange=10, Maxpip=500):
         # Bóng của nến tăng.
+        # dtype = {"open":float, "high":float, "low":float, "close":float, "tick_volume":int, "spread":int, "real_volume":int}
 
-        dtype = {"open":float, "high":float, "low":float, "close":float, "tick_volume":int, "spread":int, "real_volume":int}
         UpperCondition = (self.pdReader["open"] - self.pdReader["close"])> 0
-        
         UpperCandle = self.pdReader.where(UpperCondition).dropna()#.astype(dtype=dtype)
         UpperShadows = pd.DataFrame((UpperCandle["high"] - UpperCandle["close"])*10000)
         LowerShadows = pd.DataFrame((UpperCandle["open"] - UpperCandle["low"])*10000)
-        
+
         # Tính số râu nến thông dụng
         start = end =0
-        scope = {}
-        for i in range(0, Maxpip, CandleRange): 
-            scope[str(i) + "-" + str(i+CandleRange)] = None
-            
-        editdata = LowerShadows[0].astype(int) // CandleRange * CandleRange    
-        print(editdata.value_counts())
-        for i in UpperShadows[0]:
-            start = (int(round(i)) // CandleRange) * CandleRange
-            end = start + CandleRange
-            sectionVol = str(start) + "-" + str(end)
-            if (scope.get(sectionVol) is None): #(sectionVol not in scope) or 
-                scope[sectionVol] = 0
-            scope[sectionVol] += 1
-        se1 = pd.Series(data=scope.values(), index=scope.keys())
-        df = {'increase_up_sd': se1, 'increase_down_sd': se1, 'decrease_up_sd': se1, 'decrease_down_sd': se1}
-        print(pd.DataFrame(df).dropna().astype(int))
+        scope_up = scope_down = pd.DataFrame()
+        scope_up = UpperShadows[0].astype(int) // CandleRange * CandleRange    
+        scope_down = LowerShadows[0].astype(int) // CandleRange * CandleRange
 
+        df = {self.CU_up_name: scope_up.value_counts(), self.CU_down_name: scope_down.value_counts()}
+
+        return pd.DataFrame(df).fillna(0)
+
+    def DecreaseShadows_Analystic(self, CandleRange=10, Maxpip=500):
+        # Bóng nến giảm index là khoảng số pip
+        # Trả về bảng các giá trị phổ biến của râu nến, các giá trị trong khoảng [index+CandleRange] VD: Index=0 -> giá trị từ 0-10
+        UpperCondition = (self.pdReader["open"] - self.pdReader["close"]) <= 0
+        LowerCandle = self.pdReader.where(UpperCondition).dropna()
+        UpperShadows = pd.DataFrame((LowerCandle["high"] - LowerCandle["close"])*10000)
+        LowerShadows = pd.DataFrame((LowerCandle["open"] - LowerCandle["low"])*10000)
+
+        # Tính số râu nến thông dụng
+        scope_up = scope_down = pd.DataFrame()
+
+        scope_up = UpperShadows[0].astype(int) // CandleRange * CandleRange    
+        scope_down = LowerShadows[0].astype(int) // CandleRange * CandleRange
+
+        df = {self.CL_up_name: scope_up.value_counts(), self.CL_down_name: scope_down.value_counts()}
+        scope = pd.DataFrame(df).fillna(0)
         # print(scope)
-        return
+        return scope
 
-    def LowerShadows_Analystic(self, CandleStickList):
-        # Bóng nến giảm
-        return
-    def Shadows_Analystic(self, rangecandle = 10, maxpip=500):
+    def CandleShadows_Analystic(self, rangecandle = 10, maxpip=500):
+
+        dtype = {self.CL_up_name:int, self.CL_down_name:int, self.CU_up_name:int, self.CU_down_name:int}
+        scope1 = self.IncreaseShadows_Analystic()
+        scope2 = self.DecreaseShadows_Analystic()
+
+        result = pd.concat([scope1, scope2], axis=1).fillna(0).astype(dtype)
+        print(result)
+
         return
 
     def CandleBody_Analystic(self, CandleStickList):
@@ -110,7 +121,7 @@ def main():
             #print(subpd.to_string())
             # Volume analystic
             anal = DataAnalystic(pathfile)
-            anal.UpperShadows_Analystic()
+            anal.CandleShadows_Analystic()
 
             #anal.Volume_Analystic(df['tick_volume'].to_list())
             #anal.UpperShadows_Analystic(df['tick_volume'].to_list())
